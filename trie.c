46d5a5d71f9e45f8b9c0502754967fdedb9976fb
João Eduardo Zangari Ambrosio <129245173+dioni04@users.noreply.github.com>
2024-05-17 17:29:54 -0300

    Update trie.c
    Função nova para indice na Trie para nova ordem da Carmem

--- trie.c
+++ trie.c
@@ -1,5 +1,23 @@
 #include "trie.h"
 
+//Define indice do caracter no Nodo
+int indC(char c){
+    if(c == 0)
+        return 0;
+
+    if(c == ' ')
+        return 1;
+
+    if(c >= '0' && c <= '9')
+        return 2 + (c - '0');
+
+    c = tolower(c); //Transforma em caracter minusculo
+    if(c >= 'a' && c <= 'z')
+        return 12 + (c - 'a');
+
+    return MAX-1; //Caracter desconhecido ?
+}
+
 //Abre o arquivo de dados, e copia suas informacoes p/ a arv
 node* processaArq(char *path, node* raiz) {
 
